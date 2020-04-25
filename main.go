264510b88619fa599f9cfe7c513e2594dcbadb24
ryan <26235793+rbxb@users.noreply.github.com>
2020-04-24 19:45:16 -0700

    Update main.go

--- main.go
+++ main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/rbxb/httpfilter"
 )
 
 func main() {
-	sv := NewPlaceServer(256, 256, 16)
-	filter := httpfilter.NewServer("./root", map[string]httpfilter.OpFunc{
-		"place-api": func(w http.ResponseWriter, req *http.Request, query string, args []string) string {
-			sv.ServeHTTP(w, req)
-			return query
-		},
-	})
-	http.ListenAndServe(":8080", filter)
+	sv := NewServer(512, 512, 32, 32, 32)
+	http.ListenAndServe(":8080", sv)
 }
