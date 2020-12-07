2cb70fc4d8536e00a78351698af2660851d63423
ryan <26235793+rbxb@users.noreply.github.com>
2020-12-06 21:24:17 -0600

    improve rate limiter
    handle network anomalies better

--- server.go
+++ server.go
@@ -111,13 +111,21 @@ func (sv *Server) getConnIndex() int {
 }
 
 func rateLimiter() func() bool {
-	const rate = 6   //per second average
-	const min = 0.01 //kick threshold
+	const rate = 6   // per second average
+	const min = 0.01 // kick threshold
+
+	// Minimum time difference between messages
+	// Network sometimes delivers two messages in quick succession
+	const minDif = int64(time.Millisecond)
+
 	last := time.Now().UnixNano()
-	var v float32 = 1.0
 	return func() bool {
 		now := time.Now().UnixNano()
-		v *= float32((now-last)*rate) / float32(time.Second)
+		dif := now - last
+		if dif < minDif {
+			dif = minDif
+		}
+		v := float32(rate*dif) / float32(time.Second)
 		if v > 1.0 {
 			v = 1.0
 		}
