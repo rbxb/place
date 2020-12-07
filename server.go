7a11661cc258e035e97a2ab9de7e650a286079a5
ryan <26235793+rbxb@users.noreply.github.com>
2020-12-06 21:58:21 -0600

    fixing rate limit

--- server.go
+++ server.go
@@ -111,25 +111,27 @@ func (sv *Server) getConnIndex() int {
 }
 
 func rateLimiter() func() bool {
-	const rate = 6   // per second average
+	const rate = 8   // per second average
 	const min = 0.01 // kick threshold
 
 	// Minimum time difference between messages
 	// Network sometimes delivers two messages in quick succession
-	const minDif = int64(time.Microsecond * 10)
+	const minDif = int64(time.Millisecond * 50)
 
 	last := time.Now().UnixNano()
+	var v float32 = 1.0
 	return func() bool {
 		now := time.Now().UnixNano()
 		dif := now - last
 		if dif < minDif {
 			dif = minDif
 		}
-		v := float32(rate*dif) / float32(time.Second)
+		v *= float32(rate*dif) / float32(time.Second)
 		if v > 1.0 {
 			v = 1.0
 		}
 		last = now
+		fmt.Println(v)
 		return v > min
 	}
 }
